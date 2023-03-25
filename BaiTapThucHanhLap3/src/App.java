4588093da7fab00bca348472c4c699d5636ce786
tungdinhle <125621401+tungdinhle@users.noreply.github.com>
2023-03-25 23:58:14 +0700

    bai tap 23/03

---
+++ BaiTapThucHanhLap3/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    /**
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        HinhTruTron htt = new HinhTruTron();
+        htt.BanKinh = 5f;
+        htt.ChieuCao = 2f;
+        htt.inThongTin();
+   
+    }
+}
+
